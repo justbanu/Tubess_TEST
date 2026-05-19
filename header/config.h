9a5caacdcc9963dd3597d9638fb594458739c41b
Your Name <banuagilschool@gmail.com>
2026-05-20 01:11:10 +0700

    fix: adjust system integration

--- header/config.h
+++ header/config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-/* ========== BROWSER CONSTANTS ========== */
+/* Browser */
 #define CACHE_MAX_AMOUNT     10
 #define TABS_MAX_AMOUNT      10
 #define DOWNLOAD_MAX_AMOUNT  5
@@ -11,15 +11,15 @@
 #define MAX_HISTORY_LEN      100
 #define MAX_LINKED_PAGES     50
 
-/* ========== ANSI COLOR CODES ========== */
-#define COLOR_RESET   "\033[0m"
-#define COLOR_RED     "\033[1;31m"
-#define COLOR_GREEN   "\033[1;32m"
-#define COLOR_YELLOW  "\033[1;33m"
-#define COLOR_BLUE    "\033[1;34m"
-#define COLOR_MAGENTA "\033[1;35m"
-#define COLOR_CYAN    "\033[1;36m"
-#define COLOR_BOLD    "\033[1m"
-#define COLOR_DIM     "\033[2m"
+/*  ANSI COLOR CODES  */
+#define COLOR_RESET   "\033[0m"     // mengembalikan warna/style ke normal
+#define COLOR_RED     "\033[1;31m"  // teks merah terang / bold
+#define COLOR_GREEN   "\033[1;32m"  // teks hijau terang / bold
+#define COLOR_YELLOW  "\033[1;33m"  // teks kuning terang / bold
+#define COLOR_BLUE    "\033[1;34m"  // teks biru terang / bold
+#define COLOR_MAGENTA "\033[1;35m"  // teks magenta/ungu terang / bold
+#define COLOR_CYAN    "\033[1;36m"  // teks cyan/biru muda terang / bold
+#define COLOR_BOLD    "\033[1m"     // teks tebal/bold
+#define COLOR_DIM     "\033[2m"     // teks redup/dim
 
-#endif /* CONFIG_H */
+#endif 
